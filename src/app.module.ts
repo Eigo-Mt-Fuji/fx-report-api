e959724fbadfa3168adffc04962a92d17b8ed5df
user.email <efg.river@gmail.com>
2022-04-09 20:43:34 +0900

    fix aws-serverless entrypoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { PostsController } from './posts/posts.controller';
 import { FxAnalysisController } from './fx-analysis/fx-analysis.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ envFilePath: ['.env.local', '.env'] })],
   controllers: [AppController, UsersController, PostsController, FxAnalysisController],
   providers: [AppService],
 })
